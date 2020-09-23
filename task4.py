a58dc4e022ca77e4b77e361d312b470e4731de67
sambernn <55156541+sambernn@users.noreply.github.com>
2020-09-23 13:04:07 -0700

    Update task4.py

--- task4.py
+++ task4.py
@@ -13,3 +13,16 @@
 #
 # Test output
 # input sides of 5 and 7 should give hypotenuse of 8.60232526704
+import math
+
+print("Input side 1")
+s1 = input()
+s1 = int(s1)
+print("Input side 2")
+s2 = input()
+s2 = int(s2)
+hyp2 = math.pow(s1,2) + math.pow(s2,2)
+hyp = math.sqrt(hyp2)
+
+print("Hypotenuse = " ,end="")
+print(hyp)
